2fa1268e1bced08d386e1c7f011c7f0d02e4e861
YVeselovskyi <19dynamokyiv94@gmail.com>
2016-10-02 19:29:46 +0300

    Another try to fix loop

--- app.js
+++ app.js
@@ -64,50 +64,47 @@ function receivedMessage(event) {
   var messageText = message.text;
   var messageAttachments = message.attachments;
 
-  // allSenders[senderID] = true;
+  allSenders[senderID] = true;
+
+  Object.keys(allSenders).forEach(function(senderID) {
+      sendTextMessage(senderID, messageText)
+  });
+  console.log(allSenders);
+
+  // var messageId = message.mid;
   //
-  // Object.keys(allSenders).forEach(function(senderID) {
-  //     sendTextMessage(senderID, messageText)
-  // });
-  // console.log(allSenders);
-  // console.log("Received message for user %d and page %d at %d with message:",
-  //   senderID, recipientID, timeOfMessage);
-  // console.log(JSON.stringify(message));
-
-  var messageId = message.mid;
-
-  // You may get a text or attachment but not both
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-
-  if (messageText) {
-
-    // If we receive a text message, check to see if it matches any special
-    // keywords and send back the corresponding example. Otherwise, just echo
-    // the text we received.
-    switch (messageText) {
-      case 'image':
-        sendImageMessage(senderID);
-        break;
-
-      case 'button':
-        sendButtonMessage(senderID);
-        break;
-
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      case 'receipt':
-        sendReceiptMessage(senderID);
-        break;
-
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
+  // // You may get a text or attachment but not both
+  // var messageText = message.text;
+  // var messageAttachments = message.attachments;
+  //
+  // if (messageText) {
+  //
+  //   // If we receive a text message, check to see if it matches any special
+  //   // keywords and send back the corresponding example. Otherwise, just echo
+  //   // the text we received.
+  //   switch (messageText) {
+  //     case 'image':
+  //       sendImageMessage(senderID);
+  //       break;
+  //
+  //     case 'button':
+  //       sendButtonMessage(senderID);
+  //       break;
+  //
+  //     case 'generic':
+  //       sendGenericMessage(senderID);
+  //       break;
+  //
+  //     case 'receipt':
+  //       sendReceiptMessage(senderID);
+  //       break;
+  //
+  //     default:
+  //       sendTextMessage(senderID, messageText);
+  //   }
+  // } else if (messageAttachments) {
+  //   sendTextMessage(senderID, "Message with attachment received");
+  // }
 }
 
 function sendTextMessage(recipientId, messageText) {
