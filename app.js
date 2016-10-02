baf5ee49f3454219f9608d9aa8408baa071f2c78
YVeselovskyi <19dynamokyiv94@gmail.com>
2016-10-02 19:37:40 +0300

    Messaging event if statement

--- app.js
+++ app.js
@@ -8,139 +8,108 @@ const pageToken = 'EAAMy7RcgMngBAKuBeZBkeRocU4TbaBytzYU2Tx9xexoDQDfmR1XEdEayBPJX
 
 app.use(bodyParser.json())
 
-app.get('/' , (req,res) => {
-  res.send('Main!');
+app.get('/', (req, res) => {
+    res.send('Main!');
 })
 
 
 app.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'DynamoKyiv') {
-    // console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    // console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === 'DynamoKyiv') {
+        // console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        // console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
 });
 
 var allSenders = {};
 
-app.post('/webhook', function (req, res) {
-  var data = req.body;
+app.post('/webhook', function(req, res) {
+    var data = req.body;
 
-  // Make sure this is a page subscription
-  if (data.object == 'page') {
-    // Iterate over each entry
-    // There may be multiple if batched
-    data.entry.forEach(function(pageEntry) {
-      var pageID = pageEntry.id;
-      var timeOfEvent = pageEntry.time;
-      // Iterate over each messaging event
-      pageEntry.messaging.forEach(function(messagingEvent) {
-        if (messagingEvent.optin) {
-          receivedAuthentication(messagingEvent);
-        } else if (messagingEvent.message) {
-          console.log('MESSAGING EVENT');
-          receivedMessage(messagingEvent);
-        } else {
-          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-        }
-      });
-    });
-    res.sendStatus(200);
-  }
+    // Make sure this is a page subscription
+    if (data.object == 'page') {
+        // Iterate over each entry
+        // There may be multiple if batched
+        data.entry.forEach(function(pageEntry) {
+            var pageID = pageEntry.id;
+            var timeOfEvent = pageEntry.time;
+            // Iterate over each messaging event
+            pageEntry.messaging.forEach(function(messagingEvent) {
+                if (messagingEvent.optin) {
+                    receivedAuthentication(messagingEvent);
+                } else if (messagingEvent.message) {
+                    receivedMessage(messagingEvent);
+                } else {
+                    console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+                }
+            });
+        });
+        res.sendStatus(200);
+    }
 });
 
 
 function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
 
-  var messageId = message.mid;
+    var messageId = message.mid;
 
-  // You may get a text or attachment but not both
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
+    // You may get a text or attachment but not both
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
 
-  // allSenders[senderID] = true;
-  //
-  // Object.keys(allSenders).forEach(function(senderID) {
-  //     sendTextMessage(senderID, messageText)
-  // });
-  // console.log(allSenders);
+    allSenders[senderID] = true;
 
-  var messageId = message.mid;
+    if (message && messageText) {
+        Object.keys(allSenders).forEach(function(senderID) {
+            sendTextMessage(senderID, messageText)
+        });
+        console.log(allSenders);
+    };
 
-  // You may get a text or attachment but not both
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-
-  if (messageText) {
-
-    // If we receive a text message, check to see if it matches any special
-    // keywords and send back the corresponding example. Otherwise, just echo
-    // the text we received.
-    switch (messageText) {
-      case 'image':
-        sendImageMessage(senderID);
-        break;
-
-      case 'button':
-        sendButtonMessage(senderID);
-        break;
-
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      case 'receipt':
-        sendReceiptMessage(senderID);
-        break;
-
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
 }
 
 function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: messageText
+        }
+    };
 
-  callSendAPI(messageData);
+    callSendAPI(messageData);
 }
 
 function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: pageToken },
-    method: 'POST',
-    json: messageData
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: pageToken
+        },
+        method: 'POST',
+        json: messageData
 
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
 
-      // console.log("Successfully sent generic message with id %s to recipient %s",
-      //   messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      // console.error(response);
-      // console.error(error);
-    }
-  });
+            // console.log("Successfully sent generic message with id %s to recipient %s",
+            //   messageId, recipientId);
+        } else {
+            console.error("Unable to send message.");
+            // console.error(response);
+            // console.error(error);
+        }
+    });
 }
 
 
