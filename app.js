9692209ff12ddd0ac729c0d1a36c55884c3abe17
Yaroslav Veselovskyi <19dynamokyiv94@gmail.com>
2016-10-05 15:16:20 +0300

    Console.log event

--- app.js
+++ app.js
@@ -65,6 +65,8 @@ app.post('/webhook', (req, res) => {
     let events = req.body.entry[0].messaging;
     for (let i = 0; i < events.length; i++) {
         let event = events[i];
+        console.log(event.message + 'looool');
+        console.log(event.message.text + 'keeeek');
         if (event.message && event.message.text) {
             sendMessage(event.sender.id, {
                 text: "Добрый день! Список команд есть в меню слева :)"
