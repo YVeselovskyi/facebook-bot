147447e9d4f24816c60a9c26f46fd84453107dee
Yaroslav Veselovskyi <19dynamokyiv94@gmail.com>
2016-10-05 15:22:57 +0300

    Console.log event 3

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.post('/webhook', (req, res) => {
     let events = req.body.entry[0].messaging;
     for (let i = 0; i < events.length; i++) {
         let event = events[i];
-        console.log(event + 'looool');
+        console.log(event);
         console.log(event.message.text + 'keeeek');
         if (event.message && event.message.text) {
             sendMessage(event.sender.id, {
