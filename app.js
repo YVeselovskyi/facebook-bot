7b51dda175a090ee3a7e57decdaf31e8248d4a63
Yaroslav Veselovskyi <19dynamokyiv94@gmail.com>
2016-10-05 16:41:26 +0300

    Old post test - 3

--- app.js
+++ app.js
@@ -62,28 +62,6 @@ const fbMessage = {
 };
 
 // Handler receiving messages
-// app.post('/webhook', (req, res) => {
-//     let events = req.body.entry[0].messaging;
-//     for (let i = 0; i < events.length; i++) {
-//         let event = events[i];
-//         if (event.message && event.message.text) {
-//             sendMessage(event.sender.id, {
-//                 text: "Добрый день! Список команд есть в меню слева :)"
-//             });
-//             // if user sends postback
-//         } else if (event.postback.payload == 'news') {
-//             fbMessage.getRandomNewsItem(event.sender.id);
-//         } else if (event.postback.payload == 'cinema') {
-//             fbMessage.getAllFilms(event.sender.id);
-//         } else if (event.postback.payload == 'theatre') {
-//             sendTheatreImage(event.sender.id);
-//         } else {
-//             fbMessage.getAllConcerts(event.sender.id);
-//         }
-//     }
-//     res.sendStatus(200);
-// });
-
 app.post('/webhook', (req, res) => {
     let events = req.body.entry[0].messaging;
     for (let i = 0; i < events.length; i++) {
@@ -96,8 +74,14 @@ app.post('/webhook', (req, res) => {
                 });
             }
             // if user sends postback
-        } else if (event.postback) {
-            sendInfo(event.sender.id, event.postback.payload);
+        } else if (event.postback.payload == 'news') {
+            fbMessage.getRandomNewsItem(event.sender.id);
+        } else if (event.postback.payload == 'cinema') {
+            fbMessage.getAllFilms(event.sender.id);
+        } else if (event.postback.payload == 'theatre') {
+            sendTheatreImage(event.sender.id);
+        } else {
+            fbMessage.getAllConcerts(event.sender.id);
         }
     }
     res.sendStatus(200);
